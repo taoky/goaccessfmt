17baa0b1d05d2a41a76ca12bf64b25cf9955740c
taoky <me@taoky.moe>
2024-08-22 13:20:01 +0800

    LICENSE

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # goaccessfmt
 
-A best-effort pure-go implementation of parser.c in goaccess.
+A best-effort pure-go implementation of parser.c in [goaccess](https://goaccess.io/).
+
+Reference program extracted from goaccess code: [assets/reference.c](assets/reference.c). Obviously this program uses code from goaccess project (MIT).
--- cmd/example/main.go
+++ cmd/example/main.go
@@ -12,7 +12,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	conf := goaccessfmt.SetupConfig(logfmt, datefmt, timefmt, time.FixedZone("UTC+8", 8*60*60))
+	conf, err := goaccessfmt.SetupConfig(logfmt, datefmt, timefmt, time.FixedZone("UTC+8", 8*60*60))
+	if err != nil {
+		panic(err)
+	}
 	var logitem goaccessfmt.GLogItem
 
 	line := `114.5.1.4 - - [11/Jun/2023:11:23:45 +0800] "GET /example/path/file.img HTTP/1.1" 429 568 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"`
@@ -29,7 +32,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	conf = goaccessfmt.SetupConfig(logfmt, datefmt, timefmt, time.FixedZone("UTC+8", 8*60*60))
+	conf, err = goaccessfmt.SetupConfig(logfmt, datefmt, timefmt, time.FixedZone("UTC+8", 8*60*60))
+	if err != nil {
+		panic(err)
+	}
 	logitem = goaccessfmt.GLogItem{}
 	line = `{"level":"info","ts":1646861401.5241024,"logger":"http.log.access","msg":"handled request","request":{"remote_ip":"127.0.0.1","remote_port":"41342","client_ip":"127.0.0.1","proto":"HTTP/2.0","method":"GET","host":"localhost","uri":"/","headers":{"User-Agent":["curl/7.82.0"],"Accept":["*/*"],"Accept-Encoding":["gzip, deflate, br"]},"tls":{"resumed":false,"version":772,"cipher_suite":4865,"proto":"h2","server_name":"example.com"}},"bytes_read": 0,"user_id":"","duration":0.000929675,"size":10900,"status":200,"resp_headers":{"Server":["Caddy"],"Content-Encoding":["gzip"],"Content-Type":["text/html; charset=utf-8"],"Vary":["Accept-Encoding"]}}`
 	fmt.Println(line)
